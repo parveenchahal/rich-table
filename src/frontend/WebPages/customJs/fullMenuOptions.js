82f9631f86e1dfac4693f27829bf295f3bc4b7c1
bhawna2303 <bhawna.kumari.nitp@gmail.com>
2020-05-25 04:44:14 +0530

    adding menu

--- src/frontend/WebPages/customJs/fullMenuOptions.js
+++ src/frontend/WebPages/customJs/fullMenuOptions.js
@@ -64,7 +64,7 @@ function init() {
         },
       ]
     },
-    {
+    /*{
       "display": "Burgers and Sandwiches",
       "list" : [
         {"name" : "",
@@ -189,7 +189,7 @@ function init() {
          "price" : ""
         }
       ]
-    }
+    }*/
   ]
 
   var out = "";
@@ -204,5 +204,29 @@ function init() {
   }
   document.getElementById("v-pills-tab").innerHTML = out;
 
+  var outcontent;
+  j = 1;
+  var k;
+  for (i = 0; i < arr.length; i++) {
+    outcontent = "";
+    for(k =0; k < arr[i].list.length; k++){     			
+       outcontent += '<div class="col-md-4 text-center">' +
+                    '<div class="menu-wrap">' + 
+                    '<a href="#" class="menu-img img mb-4" style="background-image: url(images/pizza-1.jpg);"></a>' +
+                    '<div class="text">' +
+                    '<h3><a href="#">' + arr[i].list[k].name +'</a></h3>' +
+                    '<p>' + arr[i].list[k].desc + '</p>' +
+                    '<p class="price"><span>' + arr[i].list[k].price + '</span></p>' +
+                    '<p><a href="#" class="btn btn-white btn-outline-white">Add to cart</a></p>' +
+                    '</div></div></div>';     
+    }
+    outcontent = '<div class="row">' + outcontent + '</div>';
+    var tabid = "v-pills-" + j;
+    document.getElementById(tabid).innerHTML = outcontent;
+    j++;
+
+  }
+  
+
 }
-document.getElementById("v-pills-tab").onload = init();
+document.getElementById("tab-n-content").onload = init();
