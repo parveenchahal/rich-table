ce1fa247e72677ec292c270600e8be4c4d4b1ee0
bhawna2303 <bhawna.kumari.nitp@gmail.com>
2020-05-25 05:39:52 +0530

    full menu

--- src/frontend/WebPages/customJs/fullMenuOptions.js
+++ src/frontend/WebPages/customJs/fullMenuOptions.js
@@ -8,60 +8,147 @@ function init() {
          "desc" : "Veggie House Pizza[9 Inches]+Maharaja Burger+Paneer Sandwich+Veg Garlic Bread+Coke[300 ml]",
          "price" : "₹665"
         },
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
+        {"name" : "Super Combo",
+         "desc" : "Mexican Maggi With Cheese+Paneer Onion Pizza[9 Inches]+Chilli Paneer Burger+Cold Coffee",
+         "price" : "₹500"
         },
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
+        {"name" : "Onion Pizza[7 Inches]+Corn Pizza[7 Inches]",
+         "desc" : "Lots of Cheese, onion & corn",
+         "price" : "₹120"
         },
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
+        {"name" : "Capsicum Pizza[7 Inches]+Tomato Pizza[7 Inches]",
+         "desc" : "Lots of Cheese, Capsicum and Tomato.",
+         "price" : "₹110"
         }
      ]
     },
     {
       "display": "Build Your Own Pizza",
       "list" : [
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
+        {"name" : "Build Your Own Pizza with Two Toppings",
+         "desc" : "Get a pizza of your own choice. Choose any 2 toppings",
+         "price" : "₹100"
         },
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
+        {"name" : "Build Your Own Pizza with Three Toppings",
+         "desc" : "Get a pizza of your own choice. Choose any 3 toppings and get ... more",
+         "price" : "₹125"
         },
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
-        },
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
-        },
+        {"name" : "Build Your Own Pizza with Four Toppings",
+         "desc" : "Get a pizza of your own choice. Choose any 4 toppings and get ... more",
+         "price" : "₹150"
+        }
       ]
     },
     {
       "display": "Pizza and Pasta",
       "list" : [
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
+        {"name" : "Tomato Pizza",
+         "desc" : "Lots of cheese, tomato.",
+         "price" : "₹65"
         },
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
+        {"name" : "Onion Pizza",
+         "desc" : "Lots of cheese, Onion.",
+         "price" : "₹65"
         },
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
+        {"name" : "Capsicum Pizza",
+         "desc" : "Lots of cheese, capsicum.",
+         "price" : "₹65"
         },
-        {"name" : "",
-         "desc" : "",
-         "price" : ""
+        {"name" : "Corn Pizza",
+         "desc" : "Lots of cheese, corn.",
+         "price" : "₹65"
         },
+        {"name" : "Tomato Pizza",
+         "desc" : "Lots of cheese, tomato.",
+         "price" : "₹65"
+        },
+        {"name" : "Onion Pizza",
+         "desc" : "Lots of cheese, Onion.",
+         "price" : "₹65"
+        },
+        {"name" : "Capsicum Pizza",
+         "desc" : "Lots of cheese, capsicum.",
+         "price" : "₹65"
+        },
+        {"name" : "Corn Pizza",
+         "desc" : "Lots of cheese, corn.",
+         "price" : "₹65"
+        }
+      ]
+    },
+    {
+      "display": "Combos",
+      "list" : [
+        {"name" : "Super Duper Combo",
+         "desc" : "Veggie House Pizza[9 Inches]+Maharaja Burger+Paneer Sandwich+Veg Garlic Bread+Coke[300 ml]",
+         "price" : "₹665"
+        },
+        {"name" : "Super Combo",
+         "desc" : "Mexican Maggi With Cheese+Paneer Onion Pizza[9 Inches]+Chilli Paneer Burger+Cold Coffee",
+         "price" : "₹500"
+        },
+        {"name" : "Onion Pizza[7 Inches]+Corn Pizza[7 Inches]",
+         "desc" : "Lots of Cheese, onion & corn",
+         "price" : "₹120"
+        },
+        {"name" : "Capsicum Pizza[7 Inches]+Tomato Pizza[7 Inches]",
+         "desc" : "Lots of Cheese, Capsicum and Tomato.",
+         "price" : "₹110"
+        }
+     ]
+    },
+    {
+      "display": "Build Your Own Pizza",
+      "list" : [
+        {"name" : "Build Your Own Pizza with Two Toppings",
+         "desc" : "Get a pizza of your own choice. Choose any 2 toppings",
+         "price" : "₹100"
+        },
+        {"name" : "Build Your Own Pizza with Three Toppings",
+         "desc" : "Get a pizza of your own choice. Choose any 3 toppings and get ... more",
+         "price" : "₹125"
+        },
+        {"name" : "Build Your Own Pizza with Four Toppings",
+         "desc" : "Get a pizza of your own choice. Choose any 4 toppings and get ... more",
+         "price" : "₹150"
+        }
+      ]
+    },
+    {
+      "display": "Pizza and Pasta",
+      "list" : [
+        {"name" : "Tomato Pizza",
+         "desc" : "Lots of cheese, tomato.",
+         "price" : "₹65"
+        },
+        {"name" : "Onion Pizza",
+         "desc" : "Lots of cheese, Onion.",
+         "price" : "₹65"
+        },
+        {"name" : "Capsicum Pizza",
+         "desc" : "Lots of cheese, capsicum.",
+         "price" : "₹65"
+        },
+        {"name" : "Corn Pizza",
+         "desc" : "Lots of cheese, corn.",
+         "price" : "₹65"
+        },
+        {"name" : "Tomato Pizza",
+         "desc" : "Lots of cheese, tomato.",
+         "price" : "₹65"
+        },
+        {"name" : "Onion Pizza",
+         "desc" : "Lots of cheese, Onion.",
+         "price" : "₹65"
+        },
+        {"name" : "Capsicum Pizza",
+         "desc" : "Lots of cheese, capsicum.",
+         "price" : "₹65"
+        },
+        {"name" : "Corn Pizza",
+         "desc" : "Lots of cheese, corn.",
+         "price" : "₹65"
+        }
       ]
     },
     /*{
@@ -197,18 +284,26 @@ function init() {
   var i;
   var j =1;
   for (i = 0; i < arr.length; i++) {
-    out += '<a class="nav-link" id="v-pills-' + j + '-tab" data-toggle="pill" href="#v-pills-' + j + '" role="tab" aria-controls="v-pills-' + j + '" aria-selected="true">' +
+    if(i == 0){
+      out += '<a class="nav-link active" id="v-pills-' + j + '-tab" data-toggle="pill" href="#v-pills-' + j + '" role="tab" aria-controls="v-pills-' + j + '" aria-selected="true">' +
        arr[i].display +
       '</a>';
-      j++;
+    }
+    else{
+      out += '<a class="nav-link" id="v-pills-' + j + '-tab" data-toggle="pill" href="#v-pills-' + j + '" role="tab" aria-controls="v-pills-' + j + '" aria-selected="true">' +
+       arr[i].display +
+      '</a>';
+    }    
+    j++;
   }
   document.getElementById("v-pills-tab").innerHTML = out;
 
+  var totalcontent = "";
   var outcontent;
   j = 1;
   var k;
-  for (i = 0; i < arr.length; i++) {
-    outcontent = "";
+  for (i = 0; i < arr.length; i++) {   
+    outcontent = ""; 
     for(k =0; k < arr[i].list.length; k++){     			
        outcontent += '<div class="col-md-4 text-center">' +
                     '<div class="menu-wrap">' + 
@@ -220,12 +315,17 @@ function init() {
                     '<p><a href="#" class="btn btn-white btn-outline-white">Add to cart</a></p>' +
                     '</div></div></div>';     
     }
-    outcontent = '<div class="row">' + outcontent + '</div>';
-    var tabid = "v-pills-" + j;
-    document.getElementById(tabid).innerHTML = outcontent;
+    if( i == 0){
+      outcontent = '<div class="tab-pane fade show active" id="v-pills-' + j + '" role="tabpanel" aria-labelledby="v-pills-' + j + '-tab"><div class="row">' + outcontent + '</div></div>';
+    }
+    else{
+      outcontent = '<div class="tab-pane fade " id="v-pills-' + j + '" role="tabpanel" aria-labelledby="v-pills-' + j + '-tab"><div class="row">' + outcontent + '</div></div>';
+    }
+    totalcontent += outcontent;
     j++;
 
   }
+  document.getElementById("v-pills-tabContent").innerHTML = totalcontent;
   
 
 }
