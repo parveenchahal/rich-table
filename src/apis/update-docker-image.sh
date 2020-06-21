106b6a48c3694594e1e098be768ab205e5a8e3ad
Ubuntu <pc@pcvm1.nak2sjuxe3medf0twl5niuwd4g.ix.internal.cloudapp.net>
2020-06-21 04:42:48 +0000

    changes

--- src/apis/update-docker-image.sh
+++ src/apis/update-docker-image.sh
@@ -3,7 +3,8 @@ if [ -z $tag ]
 then
   tag='latest'
 fi
-
+sudo docker pull pchahal24/rich-table:$tag
 sudo docker ps | grep rich-table | awk '{print $1}' | xargs sudo docker stop
-sudo docker rm rich-table && sudo docker run -d -p 5000:8080 --restart=always --name rich-table  pchahal24/rich-table:$tag
+sudo docker rm rich-table
+sudo docker run -d -p 5000:8080 --restart=always --name rich-table  pchahal24/rich-table:$tag
 
